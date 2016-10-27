5622bd4cee7307fb572340d333650c072a2bbdaa
Kiril <kiril.abashkin@gmail.com>
2016-10-27 22:04:22 +0300

    content-main changed

--- content-main.php
+++ content-main.php
@@ -1,9 +1,21 @@
 <div class="pages-stack">
-  <div class="page" id="<?php the_title(); ?>">
+  <div class="page home" id="<?php the_title(); ?>">
+
+      <div class="hero animate" data-emergence="hidden">
+          <div class="hero__text " >
+            <div><span class="hero__text--furniture">Furniture</span></div>
+            <div><span class="hero__text--with">with</span></div>
+            <div><span class="hero__text--attitude">attitude.</span></div>
+          </div>
+      </div>
+
       <header class="bp-header cf">
-        	<div class="info main">
-        		<?php the_content(); ?>
-        	</div>
+
+         <h1 class="bp-header__title" > <?php the_title(); ?> </h1>
+         <div class="info main">
+          	<?php the_content(); ?>
+         </div>
+
       </header>
     </div>
     <?php get_template_part(contact); ?>
