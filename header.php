29df2ab2906867d3689ff5b11572904ea55bcc1d
Kiril <kiril.abashkin@gmail.com>
2017-01-31 11:58:16 +0200

    updates

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 
 
 <div id="header" class="header">
-	<div class="row middle-lg">
+	<div class="row middle-lg header_inner">
 		<div class="col-lg-3 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12 start-lg center-xs center-md center-sm">
 			<a class="logo" href="<?php bloginfo('url'); ?>"> <?php bloginfo('name'); ?> </a>
 		</div style="display:inline-block; margin:0;">
@@ -44,7 +44,7 @@
 </div>
 
 <div id="header" class="header header_fixed hide">
-	<div class="row middle-lg">
+	<div class="row middle-lg header_inner">
 		<div class="col-lg-3 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12 start-lg center-xs center-md center-sm">
 			<a class="logo" href="<?php bloginfo('url'); ?>"> <?php bloginfo('name'); ?> </a>
 		</div style="display:inline-block; margin:0;">
