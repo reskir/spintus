8f6b7b31c028067124fe61ae9fa41efc12f0412a
Kiril <kiril.abashkin@gmail.com>
2017-01-31 17:09:41 +0200

    updated layout

--- content-main.php
+++ content-main.php
@@ -15,13 +15,19 @@
                 <img src="http://spintus.lt/dev/wp-content/uploads/2016/10/kitchen-1-1024x683.jpg" alt="" class="alignnone" style="width:100%" />
               </div>
               <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed accumsan turpis. Ut nec fringilla ligula, at ornare diam. Quisque scelerisque nisl nec lectus lacinia dictum. Etiam magna eros, varius vel hendrerit sit amet, scelerisque quis nibh. Donec et condimentum justo. Vestibulum tempor risus vel neque imperdiet, vitae ultricies ex sagittis. Integer non ante ante.
+                  <p class="x--p-l">
+                  Nuo matavimo ir planavimo iki užsakymo ir montavimo: visa tai — keturi žingsniai svajonių virtuvės link. Galite viską daryti patys, bet neprivalote. 
+                  Mūsų teikiamos matavimo, planavimo, finansavimo, pristatymo ir montavimo paslaugos palengvins visą procesą.
+                  </p>
               </div>
             </div>
 
             <div class="row middle-lg home_block">
               <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed accumsan turpis. Ut nec fringilla ligula, at ornare diam. Quisque scelerisque nisl nec lectus lacinia dictum. Etiam magna eros, varius vel hendrerit sit amet, scelerisque quis nibh. Donec et condimentum justo. Vestibulum tempor risus vel neque imperdiet, vitae ultricies ex sagittis. Integer non ante ante.
+              <p class="x--p-l">
+                  Nuo matavimo ir planavimo iki užsakymo ir montavimo: visa tai — keturi žingsniai svajonių virtuvės link. Galite viską daryti patys, bet neprivalote. 
+                  Mūsų teikiamos matavimo, planavimo, finansavimo, pristatymo ir montavimo paslaugos palengvins visą procesą.
+              </p>
               </div>
               <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
                 <img src="http://spintus.lt/dev/wp-content/uploads/2016/10/kitchen-1-1024x683.jpg" alt="" class="alignnone" style="width:100%" />
