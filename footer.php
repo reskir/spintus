f132ced74e2aae6daa348f107406fe49b8e275d6
Kiril <kiril.abashkin@gmail.com>
2017-02-01 16:23:45 +0200

    changes to layout and footer

--- footer.php
+++ footer.php
@@ -5,22 +5,20 @@
       })()
 	  </script>
 
-       <div class="footer">
-         <div class="row top-xs">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+       <div class="footer row top-xs">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-xs footer_block">
               <ul class="footer_list">
                 <li class="footer_list_item">Ilja Sapožnikov</li>
                 <li class="footer_list_item">+370 60507073</li>
               </ul>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 center-xs">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 center-xs footer_block">
               <p class="smaller">facebook</p>
               <p class="smaller">instagram</p>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-xs">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-xs footer_block">
               <p class="smaller">Informacija apie puslapį</p>
             </div>
-         </div>
        </div>
 
 
