337fa9c72d7d81531d94ff23111fc3fd7a13c7ae
Kiril <kiril.abashkin@gmail.com>
2017-02-01 16:12:42 +0200

    changes to main

--- templates/main.php
+++ templates/main.php
@@ -15,28 +15,46 @@
                 <img src="http://spintus.lt/dev/wp-content/uploads/2016/10/kitchen-1-1024x683.jpg" alt="" class="alignnone" style="width:100%;" />
               </div>
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                  <p class="x--p-l">
-                  Nuo matavimo ir planavimo iki užsakymo ir montavimo: visa tai — keturi žingsniai svajonių virtuvės link. Galite viską daryti patys, bet neprivalote. 
-                  Mūsų teikiamos matavimo, planavimo, finansavimo, pristatymo ir montavimo paslaugos palengvins visą procesą.
+                <div class="x--p-l">
+                  <h2> Meilė baldams </h2>
+                  <p>
+                    Čia reikėtų info apie procesus, baldų gamyba, Iljos istorija, nuo ko jis pradėjo gaminti, kodėl klientai pasirenka būtent jį
                   </p>
+                </div>
               </div>
             </div>
 
             <div class="line"></div>
 
             <div class="row middle-lg home_block">
-              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-              <p class="x--p-l">
-                  Nuo matavimo ir planavimo iki užsakymo ir montavimo: visa tai — keturi žingsniai svajonių virtuvės link. Galite viską daryti patys, bet neprivalote. 
-                  Mūsų teikiamos matavimo, planavimo, finansavimo, pristatymo ir montavimo paslaugos palengvins visą procesą.
-              </p>
+              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <div class="x--p-l">
+                  <h2> Kokybė ir kruopštumas </h2>
+                  <p>
+                     Dėl kokių detalių verta rinktis Iljos baldus ( o ne ikea), kodėl efektyvu dirbti su baldininkų, 
+                     kokia kokybę jis gali pasiūlyti (palyginus su kitom alternativom)
+                  </p>
+                </div>
               </div>
-              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
+              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" >
                 <img src="http://spintus.lt/dev/wp-content/uploads/2017/01/kitchen-1-2.jpg" alt="" class="alignnone" style="width:100%" />
               </div>
             </div>
 
-         </div>
+           <div class="line"></div>
+
+            <div class="row center-xs home_block">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+                <div class="x--p-l">
+                  <h2>Klientų atsiliepimai</h2>
+                  <p>
+                    Čia reikėtų nurodyti klietų atsiliepimus
+                  </p>
+                </div>
+              </div>
+            </div>
+
+        </div>
 
       </div>
     </div>
