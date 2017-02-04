e286dc4953a3bd5acbb09208847246dd8c8cc0a7
Kiril <kiril.abashkin@gmail.com>
2017-02-04 16:05:54 +0200

    updated reviews

--- templates/main.php
+++ templates/main.php
@@ -56,9 +56,9 @@
                     rgba(61, 66, 77, 0.1),
                     rgba(61, 66, 77, 0.3),
                     rgba(0,0,0,0.5),
-                    rgba(0,0,0,0.8)), url('<?php echo get_template_directory_uri(); ?>/images/hero-image.jpg') no-repeat center center/cover;">
+                    rgba(0,0,0,0.7)), url('<?php echo get_template_directory_uri(); ?>/images/hero-image.jpg') no-repeat center center/cover;">
                         <div class="row start-xs">
-                          <div class="col-md-6 col-xs-12 review_content">
+                          <div class="col-md-7 col-xs-12 review_content">
                             <blockquote class="review_quote">
                               Ilja atliko labai kokybišką darbą, virtuvė atitiko visus reikalavimus ir pageidavimus.
                               Buvo atsižvelgta į smulkmenas - apšvietimo integravimas, elektro laidų pravedimas ir integracija su buitine technika.
@@ -72,9 +72,9 @@
                     rgba(61, 66, 77, 0.1),
                     rgba(61, 66, 77, 0.3),
                     rgba(0,0,0,0.6),
-                    rgba(0,0,0,0.8)), url('<?php echo get_template_directory_uri(); ?>/images/example-bg-3.jpg') no-repeat center center/cover;">
+                    rgba(0,0,0,0.7)), url('<?php echo get_template_directory_uri(); ?>/images/example-bg-3.jpg') no-repeat center center/cover;">
                         <div class="row start-xs">
-                          <div class="col-md-6 col-xs-12 review_content">
+                          <div class="col-md-7 col-xs-12 review_content">
                             <blockquote class="review_quote">
                               Ilja atliko labai kokybišką darbą, virtuvė atitiko visus reikalavimus ir pageidavimus.
                               Buvo atsižvelgta į smulkmenas - apšvietimo integravimas, elektro laidų pravedimas ir integracija su buitine technika.
