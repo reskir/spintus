7879a18870b3bbff3f07d875d65c5e6ec6993b74
Kiril <kiril.abashkin@gmail.com>
2017-02-06 17:16:51 +0200

    Changed contact

--- templates/contact.php
+++ templates/contact.php
@@ -18,25 +18,37 @@
 
               
                 <?php
-                  // if the submit button is clicked, send the email
-                  if(isset($_POST['submit'])) {
-                      echo '<div>Submit</div>';
-                  }
+               add_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+                 // if the submit button is clicked, send the email
+                 if(isset($_POST['submit'])) {
+                       $to = 'kiril.abashkin@gmail.com';
+                       $subject = 'The subject';
+                       $body = 'The email body content';
+                       wp_mail( $to, $subject, $body );
+                     }
+                                      
+                       // Reset content-type to avoid conflicts -- https://core.trac.wordpress.org/ticket/23578
+                       remove_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+                       
+                       function wpdocs_set_html_mail_content_type() {
+                           return 'text/html';
+                       }
+
                 ?>
               <form method="post" class="form col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <fieldset>
                   <div class="row">
                     <h4 class="col-xs-12">Pasiteiraukite dėl savo projekto</h4>
                     <div class="input-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                      <input class="input" type="text" id="name" name="name" required />
+                      <input class="input" type="text" id="name" name="name" />
                       <label class="label" id="name" for="name">Vardas</label>
                     </div>
                     <div class="input-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                      <input class="input" type="email" id="email" name="email" required />
+                      <input class="input" type="email" id="email" name="email" />
                       <label class="label" id="email" for="email">El.paštas</label>
                     </div>
                     <div class="input-group col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                      <input class="input" type="tel" id="tel" name="tel" required />
+                      <input class="input" type="tel" id="tel" name="tel" />
                       <label class="label" id="tel" for="tel">Telefono numeris</label>
                     </div>
                   </div>
@@ -77,7 +89,7 @@
                     <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
                         <h4>Komentaras</h4>
                         <div class="input-group">
-                          <textarea class="input input--textarea" noresize required id="message" name="message"></textarea>
+                          <textarea class="input input--textarea" noresize id="message" name="message"></textarea>
                         </div>
                         <div>
                           <input  class="btn btn--success" name="submit" type="submit" value="Bendrauti" />
