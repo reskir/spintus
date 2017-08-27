02f122a75c4ea3555c09ca9c43f80ebadc79038c
Kiril <kiril.abashkin@gmail.com>
2017-08-27 20:37:58 +0300

    footer updated

--- footer.php
+++ footer.php
@@ -7,18 +7,16 @@
 
        <div class="footer row top-xs">
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-xs footer_block">
-              <ul class="footer_list">
-                <li class="footer_list_item">Ilja Sapožnikov</li>
-                <li class="footer_list_item"><?php echo get_option('telephone');?></li>
-              </ul>
+
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 center-xs footer_block">
-              <p class="smaller">facebook</p>
-              <p class="smaller">instagram</p>
+              <ul class="footer_list">
+                <li class="footer_list_item">Ilja Sapožnikov</li>
+                <li class="footer_list_item"><a href="tel:<?php echo get_option('telephone');?>"><?php echo get_option('telephone');?></a></li>
+              </ul>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 center-xs footer_block">
-              <p class="smaller">Informacija apie puslapį</p>
-              <p class="smaller"><?php echo "" . date( 'Y', current_time( 'timestamp', 1 ) );?></p>
+
             </div>
        </div>
     
