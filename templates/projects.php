88cc37ad930cc3c1b2bc09492814a9f159d7a5ca
Kiril <kiril.abashkin@gmail.com>
2017-08-27 20:25:05 +0300

    updates

--- templates/projects.php
+++ templates/projects.php
@@ -1,35 +1,8 @@
 <div class="main-content project">
   <div class="page " id="<?php the_title(); ?>">
       <div class="bp-header cf">
-          <!--<h1 class="bp-header__title center" ><?php the_title(); ?></h1>-->
         	<div class="info project-list project_container">
         		<?php the_content(); ?>
-            <!--<div class="row center-lg x--p-l">
-              <div class="col-lg-2 center-xs">
-                <div class="project-list_item">
-                  <img src="<?php echo get_template_directory_uri(); ?>/images/furniture/kitchen-2.svg" style="width:100%" />
-                  <h4>Virtuvės</h4>
-                </div>
-              </div>
-              <div class="col-lg-2 center-xs">
-                <div class="project-list_item">
-                  <img src="<?php echo get_template_directory_uri(); ?>/images/furniture/bed.svg" style="width:100%" />
-                  <h4>Miegamasis</h4>
-                </div>
-              </div>
-              <div class="col-lg-2 center-xs">
-                <div class="project-list_item">
-                  <img src="<?php echo get_template_directory_uri(); ?>/images/furniture/bench.svg" style="width:100%" />
-                  <h4>Lauko baldai</h4>
-                </div>
-              </div>
-              <div class="col-lg-2 center-xs">
-                <div class="project-list_item">
-                  <img src="<?php echo get_template_directory_uri(); ?>/images/furniture/table.svg" style="width:100%" />
-                  <h4>Kita</h4>
-                </div>
-              </div>
-            </div>-->
         	</div>
       </div>
     </div>
